6e0a5648a3defa19c069da0bf8ffed417999ed8f
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2024-01-11 01:42:41 +0300

    imp: determinant calculation was made

---
+++ advanced_calculations/determinant.ts
@@ -0,0 +1,18 @@
+import {isNumeric} from "../lib/isNumeric";
+
+function calculateDeterminant (matrice: number[][]): number {
+    if (!matrice.every(row => row.every(element => isNumeric(element)))) {
+        throw new Error("matrice elements must be a Number");
+    }
+    if (matrice.length !== matrice[0].length) throw new Error("matrice must be square");
+    if (matrice.length === 2 && matrice[0].length === 2) {
+        return matrice[0][0] * matrice[1][1] - matrice[0][1] * matrice[1][0];
+    }
+    let det = 0;
+    for (let i = 0; i < matrice.length; i++) {
+        const katsayi = matrice[0][i];
+        const altMatris = matrice.slice(1).map(row => row.filter((_, j) => j !== i));
+        det += katsayi * calculateDeterminant(altMatris) * (i % 2 === 0 ? 1 : -1);
+    }
+    return det;
+}
