31108b4325a8260e317363e0721738fe7a931abf
Burak Mumcu <95939910+BurakMumcu-js@users.noreply.github.com>
2024-01-16 01:26:35 +0300

    fixing again ;(

--- dist/matrices.d.ts
+++ dist/matrices.d.ts
@@ -1,1 +1,17 @@
-export {};
+import { sum } from './basic_calculations/sum';
+import { subtract } from './basic_calculations/subtraction';
+import { multiply } from './basic_calculations/multiply';
+import { determinant } from './advanced_calculations/determinant';
+import { inverse } from './advanced_calculations/inverse';
+import { power } from './advanced_calculations/power';
+import { transpose } from './advanced_calculations/transpose';
+declare const matrice: {
+    sum: typeof sum;
+    subtract: typeof subtract;
+    multiply: typeof multiply;
+    determinant: typeof determinant;
+    inverse: typeof inverse;
+    power: typeof power;
+    transpose: typeof transpose;
+};
+export = matrice;
--- dist/matrices.js
+++ dist/matrices.js
@@ -1,2 +1,18 @@
 "use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
+const sum_1 = require("./basic_calculations/sum");
+const subtraction_1 = require("./basic_calculations/subtraction");
+const multiply_1 = require("./basic_calculations/multiply");
+const determinant_1 = require("./advanced_calculations/determinant");
+const inverse_1 = require("./advanced_calculations/inverse");
+const power_1 = require("./advanced_calculations/power");
+const transpose_1 = require("./advanced_calculations/transpose");
+const matrice = {
+    sum: sum_1.sum,
+    subtract: subtraction_1.subtract,
+    multiply: multiply_1.multiply,
+    determinant: determinant_1.determinant,
+    inverse: inverse_1.inverse,
+    power: power_1.power,
+    transpose: transpose_1.transpose,
+};
+module.exports = matrice;
